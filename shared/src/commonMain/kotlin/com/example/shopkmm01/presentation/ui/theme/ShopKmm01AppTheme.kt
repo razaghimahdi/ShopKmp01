7e6b470fc75c6001819186aedfaa3eb986e8f1bd
razaghimahdi <razaghimahdi78@gmail.com>
2023-04-17 14:46:42 +0330

    - Updated ShopKmm01AppTheme.kt

--- shared/src/commonMain/kotlin/com/example/shopkmm01/presentation/ui/theme/ShopKmm01AppTheme.kt
+++ shared/src/commonMain/kotlin/com/example/shopkmm01/presentation/ui/theme/ShopKmm01AppTheme.kt
@@ -1,9 +1,10 @@
 package com.example.shopkmm01.presentation.ui.theme
 
 import androidx.compose.runtime.Composable
+import app.cash.sqldelight.db.SqlDriver
 import com.example.shopkmm01.presentation.CommonView
 
 @Composable
-internal fun ShopKmm01AppTheme(content: @Composable () -> Unit) {
-    CommonView()
+internal fun ShopKmm01AppTheme(sqlDriver: SqlDriver, content: @Composable () -> Unit) {
+    CommonView(sqlDriver = sqlDriver)
 }
