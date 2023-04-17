069c5d0641350ee0ff5daf31d1095139cb270875
razaghimahdi <razaghimahdi78@gmail.com>
2023-04-17 14:33:58 +0330

    - Updated ErrorHandling.kt

--- shared/src/commonMain/kotlin/com/example/shopkmm01/business/constants/ErrorHandling.kt
+++ shared/src/commonMain/kotlin/com/example/shopkmm01/business/constants/ErrorHandling.kt
@@ -1,6 +1,7 @@
 package com.example.shopkmm01.business.constants
 
 object ErrorHandling {
+    const val FAILED_NETWORK ="FAILED_NETWORK"
     const val GENERAL_ERROR = "GENERAL_ERROR"
     const val GENERAL_ERROR_DESC = "some error occurred, please try later!"
     const val GENERAL_ERROR_Title = "Failed!"
