1969f0ef4f34afb822988dbcbc4d791dde26db59
razaghimahdi <razaghimahdi78@gmail.com>
2023-04-17 14:46:01 +0330

    - Added new navigation

--- shared/src/commonMain/kotlin/com/example/shopkmm01/presentation/ui/screen_navigation/Navigation.kt
+++ shared/src/commonMain/kotlin/com/example/shopkmm01/presentation/ui/screen_navigation/Navigation.kt
@@ -3,8 +3,9 @@ package com.example.shopkmm01.presentation.ui.screen_navigation
 import com.example.shopkmm01.business.domain.main.Product
 
 sealed interface Navigation {
+    object SplashNavigation : Navigation
     object MainNavigation : Navigation
     data class DetailNavigation(val product: Product) : Navigation
 }
 
-data class ScreensState(val navigation: Navigation = Navigation.MainNavigation)
+data class ScreensState(val navigation: Navigation = Navigation.SplashNavigation)
