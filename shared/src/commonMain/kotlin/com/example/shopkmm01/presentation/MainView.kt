6ffa97c4187b160f6a67424be998a264d5ff22c0
razaghimahdi <razaghimahdi78@gmail.com>
2023-04-17 14:47:16 +0330

    - Updated MainView.kt

--- shared/src/commonMain/kotlin/com/example/shopkmm01/presentation/MainView.kt
+++ shared/src/commonMain/kotlin/com/example/shopkmm01/presentation/MainView.kt
@@ -1,22 +1,23 @@
 package com.example.shopkmm01.presentation
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
+import app.cash.sqldelight.db.SqlDriver
 import com.example.shopkmm01.presentation.screen.details.DetailScreen
 import com.example.shopkmm01.presentation.screen.main.MainScreen
 import com.example.shopkmm01.presentation.screen.main.MainViewModel
+import com.example.shopkmm01.presentation.screen.splash.SplashScreen
+import com.example.shopkmm01.presentation.screen.splash.SplashViewModel
 import com.example.shopkmm01.presentation.ui.screen_navigation.Navigation
 import com.example.shopkmm01.presentation.ui.screen_navigation.ScreensState
 import com.example.shopkmm01.presentation.ui.theme.ShopKmm01Theme
-import com.example.shopkmm01.presentation.ui.theme.style.ShopKmm01AppColors
 
 
 @Composable
-internal fun CommonView() {
-    val viewModel = MainViewModel()
+internal fun CommonView(sqlDriver: SqlDriver) {
+    val mainViewModel = lazy { MainViewModel(sqlDriver) }
+    val splashViewModel = lazy{ SplashViewModel(sqlDriver) }
     val screenNavigationState = remember { mutableStateOf(ScreensState()) }
 
     ShopKmm01Theme() {
@@ -26,7 +27,11 @@ internal fun CommonView() {
                 navigationState = screenNavigationState,
                 product = state.product
             )
-            Navigation.MainNavigation -> MainScreen(screenNavigationState, viewModel)
+            is Navigation.SplashNavigation -> SplashScreen(
+                navigationState = screenNavigationState,
+                splashViewModel.value
+            )
+            Navigation.MainNavigation -> MainScreen(screenNavigationState, mainViewModel.value)
         }
     }
 }
